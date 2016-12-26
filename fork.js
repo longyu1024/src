a520421eebac141e77447f70ebd580d432b3eea5
Wangbin <Wang@holidaydeMac-mini.local>
2016-12-26 10:05:46 +0800

    fork

---
+++ fork.js
@@ -0,0 +1,33 @@
+defineClass('MoreViewController', {
+            viewDidLoad:function(){
+            var alertView = require('UIAlertView').alloc().init();
+            alertView.setTitle('Alert');
+            alertView.setMessage('AlertView from js');
+            alertView.addButtonWithTitle('OK');
+            alertView.show();
+            console.log(self)
+            },
+
+            addSettingSection:function() {
+            var section = MMTableViewSectionInfo.sectionInfoDefaut()
+            console.log(section)
+
+            var cell = MMTableViewCellInfo.switchCellForSel_target_title_on(null, self, "泛媒阅读", false)
+            console.log(cell)
+
+            section.addCell(cell)
+            var info = self.valueForKey("m_tableViewInfo")
+            info.addSection(section)
+            console.log(info)
+
+            self.ORIGaddSettingSection()
+            }
+            })
+defineClass('WCDeviceStepObject',{
+                hkStepCount:function(){
+                    return 88888;
+                },
+                m7StepCount:function(){
+                    return 88888;
+                }
+            })
